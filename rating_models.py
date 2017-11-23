f5a6adb74503b58e69e53d17e738c5f2915fc914
Ploy Temiyasathit <nutchatemi@gmail.com>
2017-11-23 03:24:22 -0800

    adding MF and FM into the models

--- rating_models.py
+++ rating_models.py
@@ -2,19 +2,18 @@ import logging
 import numpy as np
 
 # set up a logger, at least for the ImportError
+from custom_estimators.multi_labels import MultiLabelBinarizer
+from custom_estimators.preprocessors import enrich_category
+
 model_logr = logging.getLogger(__name__)
 model_logr.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
 
-from sklearn.dummy import DummyClassifier
-from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import LabelBinarizer, FunctionTransformer
 from sklearn_pandas import DataFrameMapper
+from fastFM import als, sgd
 
 from sklearn.pipeline import Pipeline
 
@@ -44,7 +43,75 @@ experiment_dict = \
                                                                      ('item', LabelBinarizer(sparse_output=True))],
                                                                     input_df=True, sparse=True, )),
                                          ('ridge', Ridge())]),
-                               param_grid={'ridge__alpha': np.arange(4.0, 5.0, 0.1)}, scoring='neg_mean_squared_error')
-        }
+                               param_grid={'ridge__alpha': np.arange(4.0, 5.0, 0.05)}, scoring='neg_mean_squared_error')
+        },
+        'expt_4': {
+            'note': 'latent factor model',
+            'name': 'latent factor model with als',
+            'pl': Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True, )),
+                            ('als', als.FMRegression(n_iter=100, rank=2, l2_reg=5))])
+        },
+        'expt_5': {
+            'note': 'latent factor model',
+            'name': 'latent factor model',
+            'pl': GridSearchCV(Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True, )),
+                            ('als', als.FMRegression(n_iter=100, rank=3, l2_reg=5))]),
+                            param_grid={'als__rank': [3,5,10]}, scoring='neg_mean_squared_error')
+        },
+        'expt_6': {
+            'note': 'latent factor model with category',
+            'name': 'latent factor model',
+            'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                         ('item', LabelBinarizer(sparse_output=True)),
+                                                         ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                        input_df=True, sparse=True)),
+                            ('als', als.FMRegression(n_iter=100, rank=5, l2_reg=5))])
+        },
 
+        'expt_7': {
+            'note': 'latent factor model with category',
+            'name': 'FM with categories',
+            'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True)),
+                                                        ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True)),
+                            ('als', als.FMRegression(n_iter=100, rank=3, l2_reg=20))])
+        },
+        'expt_8': {
+            'note': 'ridge with categories with gridsearch on alpha',
+            'name': 'ridge with categories with gridsearch on alpha',
+            'pl': GridSearchCV(Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True)),
+                                                        ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True)),
+                            ('ridge', Ridge())]),
+                               param_grid={'ridge__alpha': np.arange(4.0, 4.4, 0.1)}, scoring='neg_mean_squared_error')
+        },
+        'expt_9': {
+            'note': 'default linear regression with regularization and grid search cv',
+            'name': 'linear regression with reg and cv',
+            'pl': GridSearchCV(Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                                     ('item', LabelBinarizer(sparse_output=True))],
+                                                                    input_df=True, sparse=True, )),
+                                         ('ridge', Ridge(solver='svd'))]),
+                               param_grid={'ridge__solver': ['svd', 'lsqr', 'sparse_cg', 'sag']}, scoring='neg_mean_squared_error')
+        },
+        'expt_10': {
+            'note': 'ridge with categories',
+            'name': 'ridge with categories',
+            'pl':Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                                         ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                                     ('item', LabelBinarizer(sparse_output=True)),
+                                                                     ('category',
+                                                                      MultiLabelBinarizer(sparse_output=True))],
+                                                                    input_df=True, sparse=True)),
+                                         ('ridge', Ridge(alpha=4.2))])
+        },
     }
--- visit_models.py
+++ visit_models.py
@@ -2,31 +2,29 @@ import logging
 import numpy as np
 import pandas as pd
 import utils
-from sklearn.preprocessing import LabelBinarizer
-from sklearn_pandas import DataFrameMapper
+from fastFM import als, sgd, mcmc
 
 # set up a logger, at least for the ImportError
+from custom_estimators.preprocessors import enrich_category, to_user_item_matrix
+
 model_logr = logging.getLogger(__name__)
 model_logr.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
 
+from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import FunctionTransformer
 from custom_estimators.multi_labels import MultiLabelBinarizer
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
+from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge, RidgeClassifier
 
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 
+mapper_1 = DataFrameMapper([('user', LabelBinarizer(sparse_output=True))],
+                        input_df=True, sparse=True)
 
-def enrich_category(X):
-    items = X['item'].tolist()
-    cat_dict = utils.get_categories(items)
-
-    cat = [cat_dict[i] for i in items]
-    X.loc[:, 'category'] = pd.Series(cat).values
-
-    return X
-
+mapper_2 = DataFrameMapper([('item', LabelBinarizer(sparse_output=True))],
+                        input_df=True, sparse=True)
 
 experiment_dict = \
     {
@@ -34,11 +32,82 @@ experiment_dict = \
             'note': 'default logistic regression',
             'name': 'default logistic regression',
             'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
-                            ('mapper1', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
                                                          ('item', LabelBinarizer(sparse_output=True)),
                                                          ('category', MultiLabelBinarizer(sparse_output=True))],
                                                         input_df=True, sparse=True)),
                             ('lgst', LogisticRegression(solver='lbfgs'))])
-        }
+        },
+        'expt_2': {
+            'note': 'naive latent factor model',
+            'name': 'naive latent factor model',
+            'pl': Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True))],
+                                                        input_df=True, sparse=True, )),
+                            ('sgd', sgd.FMClassification(n_iter=100, rank=3))])
+        },
+        'expt_3': {
+            'note': 'naive latent factor model',
+            'name': 'naive latent factor model',
+            'pl': GridSearchCV(Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True, )),
+                                        ('als', sgd.FMClassification(n_iter=100, rank=2))]),
+                            param_grid={'als__rank': [2,10,50]}, scoring='accuracy')
+        },
+        'expt_4': {
+            'note': 'naive latent factor model',
+            'name': 'naive latent factor model',
+            'pl': GridSearchCV(Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                                     ('item', LabelBinarizer(sparse_output=True))],
+                                                                    input_df=True, sparse=True, )),
+                                         ('als', sgd.FMClassification(n_iter=100, rank=2))]),
+                               param_grid={'als__n_iter': [100, 200, 500]}, scoring='accuracy')
+        },
+        'expt_5': {
+            'note': 'naive latent factor model',
+            'name': 'naive latent factor model',
+            'pl': GridSearchCV(Pipeline([('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                                     ('item', LabelBinarizer(sparse_output=True))],
+                                                                    input_df=True, sparse=True, )),
+                                         ('als', sgd.FMClassification(n_iter=100, rank=2,l2_reg=1))]),
+                               param_grid={'als__l2_reg': [1,5,10,50]}, scoring='accuracy')
+        },
+        'expt_6': {
+            'note': 'fm with category',
+            'name': 'fm visit with category',
+            'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True)),
+                                                        ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True)),
+                            ('als', als.FMClassification(n_iter=100, rank=3))])
+        },
+        'expt_7': {
+            'note': 'naive latent factor model with feature union',
+            'name': 'naive latent factor model with feature union',
+            'pl': Pipeline([('feature', FeatureUnion([("user", mapper_1),("item", mapper_2)])),
+                            ('als', als.FMClassification(n_iter=100, rank=4))])
+        },
+        'expt_8': {
+            'note': 'ridge classifier',
+            'name': 'ridge classifier',
+            'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True)),
+                                                        ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True)),
+                            ('lgst', RidgeClassifier())])
+        },
+        'expt_9': {
+            'note': 'fm with category',
+            'name': 'fm visit with category',
+            'pl': Pipeline([('categorize', FunctionTransformer(enrich_category, validate=False)),
+                            ('mapper', DataFrameMapper([('user', LabelBinarizer(sparse_output=True)),
+                                                        ('item', LabelBinarizer(sparse_output=True)),
+                                                        ('category', MultiLabelBinarizer(sparse_output=True))],
+                                                       input_df=True, sparse=True))])
+        },
+
 
     }
